1cad41d512c33b4141f2ee715af13f2b22a9b30d
Seepick <seepick@users.noreply.github.com>
2026-02-05 19:05:05 +0100

    detekt

--- src/main/kotlin/com/github/seepick/derbauer2/game/citizen/StarveCompute.kt
+++ src/main/kotlin/com/github/seepick/derbauer2/game/citizen/StarveCompute.kt
@@ -5,24 +5,16 @@ import com.github.seepick.derbauer2.game.common.Z
 import com.github.seepick.derbauer2.game.common.Zz
 import com.github.seepick.derbauer2.game.common.z
 import com.github.seepick.derbauer2.game.common.zz
-import io.github.oshai.kotlinlogging.KotlinLogging.logger
 
 object StarveCompute {
-    private val log = logger {}
-    fun howManyUnfed(citizens: Z, foodOwned: Zz, eatenFood: Z, eatRatio: Percent): Z {
-        // TODO refactor returns
-        log.trace { "citizens: $citizens, foodOwned: $foodOwned, eatenFood: $eatenFood, eatRatio: $eatRatio" }
-        if (citizens == 0.z) {
-            return 0.z
-        }
-        if (foodOwned <= 0.zz) {
-            return citizens
-        }
-        val foodDiff = foodOwned - eatenFood
-        log.trace { "foodDiff: $foodDiff" }
-        if (foodDiff >= 0.zz) {
-            return 0.z
-        }
+    fun howManyUnfed(citizens: Z, foodOwned: Zz, eatenFood: Z, eatRatio: Percent): Z = when {
+        citizens == 0.z -> 0.z
+        foodOwned <= 0.zz -> citizens
+        (foodOwned - eatenFood) >= 0.zz -> 0.z
+        else -> calculateUnfedCitizens(citizens, foodOwned, eatRatio)
+    }
+
+    private fun calculateUnfedCitizens(citizens: Z, foodOwned: Zz, eatRatio: Percent): Z {
         val citizensAmountFedByOneFood = eatRatio.neededToGetTo(1)
         val citizensFed = foodOwned.toZAbs() * citizensAmountFedByOneFood
         return citizens - citizensFed
